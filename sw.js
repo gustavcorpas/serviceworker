984861ef0fa3f7cbe3ce447cd66f60704a3189ff
Gustav Corpas <56586922+gustavcorpas@users.noreply.github.com>
2020-03-28 16:22:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll(['./res/404.jpg', './', './index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => {
+        return res || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('./res/404.jpg');
+      })
+  );
+
+});
